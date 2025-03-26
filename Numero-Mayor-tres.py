7f6d250f2bb73e2fd01b4fdec25c343497452be3
anonymous <76333804@continental.edu.pe>
2025-03-26 11:41:48 -0500

    Se añadio un cambio!
    cls

--- Numero-Mayor-tres.py
+++ Numero-Mayor-tres.py
@@ -1,24 +1,23 @@
 """ Determina el mayor de tres numeros ingresador por el teclado"""
 
+def intercambiar_valores(numero1,numero2):
+    temporal = numero1
+    numero1 = numero2
+    numero2 = temporal
+    return numero1, numero2
 
 numero1=float(input("Ingresa el primer numero: "))
 numero2=float(input("Ingresa el segundo numero: "))
 numero3=float(input("Ingresa el tercer numero: "))
 
 if numero1>numero2:
-    temporal=numero1
-    numero1=numero2
-    numero2=temporal
+    numero1, numero2 = intercambiar_valores(numero1,numero2)
 
 if numero2>numero3:
-    temporal=numero2
-    numero2=numero3
-    numero3=temporal
+    numero2, numero3 = intercambiar_valores(numero2, numero3)
 
-if numero3>numero1:
-    temporal=numero1
-    numero1=numero2
-    numero2=temporal
+if numero1>numero2:
+    numero1, numero2 = intercambiar_valores(numero1, numero2)
 
 print(f"numeros ordenados dos: {numero1},{numero2},{numero3}")
 print(f"el mayor es: {numero3}")
